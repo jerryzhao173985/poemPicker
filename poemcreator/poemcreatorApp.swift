f7d7f106cc92496d15630aa0767c7ca7270a7a61
Jerry Zhao <jerryzhao173985@gmail.com>
2025-01-28 09:59:26 +0800

    Initial Commit

---
+++ poemcreator/poemcreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  poemcreatorApp.swift
+//  poemcreator
+//
+//  Created by jerry on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct poemcreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
